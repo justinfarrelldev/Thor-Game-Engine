c7866cf59143d239bf75c98b054edb26d0d0f24b
VortrusDev <vortrusgame@gmail.com>
2019-10-23 04:16:41 -0400

    Added more functionality and made an even better base for games. Starting to really shape up!

--- func-lib.ts
+++ func-lib.ts
@@ -1,16 +1,56 @@
 //This is the function library for Thor. 
 //It is used for common functions and to make syntax simple. 
 
-let AddElem = (type : string, innerHTML : string) => 
+function AddElem(type : string, innerHTML : string)
+
+function AddElem(type : string, innerHTML : string, id? : string)
 {
     let el = document.createElement(type)
     el.innerHTML = innerHTML
+
+    if (id != undefined)
+    {
+        el.id = id
+    }
+
+    AppendChildToCorrectElement(el)
+
+    return el
+}
+
+function AddButton(text : string, onclick : string)
+//Shorthand
+function AddButton(text : string, onclick : any, id? : string)
+{
+    let el = document.createElement("button")
+    el.innerHTML = text
+
+    if (id != undefined)
+    {
+        el.id = id
+    }
+
+    el.onclick = onclick
+
+    AppendChildToCorrectElement(el)
+
+    return el
+}
+
+//Shorthand
+function GetElemById(id : string)
+{
+    document.getElementById(id)
+}
+
+let AppendChildToCorrectElement = (element : any) => 
+{
     if (!document.getElementById("GamePreviewWindow"))
     {
-        document.body.appendChild(el)
+        document.body.appendChild(element)
     }
     else
     {
-        document.getElementById("GamePreviewWindow").appendChild(el)
+        document.getElementById("GamePreviewWindow").appendChild(element)
     }
 }
--- js-src/func-lib.js
+++ js-src/func-lib.js
@@ -1,13 +1,35 @@
 //This is the function library for Thor. 
 //It is used for common functions and to make syntax simple. 
-let AddElem = (type, innerHTML) => {
+function AddElem(type, innerHTML, id) {
     let el = document.createElement(type);
     el.innerHTML = innerHTML;
+    if (id != undefined) {
+        el.id = id;
+    }
+    AppendChildToCorrectElement(el);
+    return el;
+}
+//Shorthand
+function AddButton(text, onclick, id) {
+    let el = document.createElement("button");
+    el.innerHTML = text;
+    if (id != undefined) {
+        el.id = id;
+    }
+    el.onclick = onclick;
+    AppendChildToCorrectElement(el);
+    return el;
+}
+//Shorthand
+function GetElemById(id) {
+    document.getElementById(id);
+}
+let AppendChildToCorrectElement = (element) => {
     if (!document.getElementById("GamePreviewWindow")) {
-        document.body.appendChild(el);
+        document.body.appendChild(element);
     }
     else {
-        document.getElementById("GamePreviewWindow").appendChild(el);
+        document.getElementById("GamePreviewWindow").appendChild(element);
     }
 };
 //# sourceMappingURL=func-lib.js.map
