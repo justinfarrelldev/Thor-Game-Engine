25bbe2c87a1eb3352fdb88b6afcc0f6092af9ae5
VortrusDev <vortrusgame@gmail.com>
2019-11-07 23:00:58 -0500

    Images can now upload to the server, but cannot be used in-game without serious tinkering. That will soon change.

--- type-lib.ts
+++ type-lib.ts
@@ -4,76 +4,141 @@
 //Currently excluded due to glitchiness
 
 
-/*
-class LinkedListNode
+if (finished === false || finished === undefined)
 {
-    /**
-     * A node for the Linked List data structure
-     */
-    /*
-    next : LinkedListNode
-    previous : LinkedListNode
-    data : any
-    constructor(next, previous, data?) {
-        this.next = next
-        this.previous = previous
+    let Global = window || global
 
-        if (data != null)
+    //Vector 2 (position x and y + magnitude)
+    class Vec2
+    {
+        /**
+         * A vector 2, which is a vector in 2 dimensional space
+         * and can be a coordinate or a direction
+         */
+        x : number
+        y : number
+        magnitude : number
+        constructor(x, y, magnitude?) {
+            this.x = x
+            this.y = y
+            if (magnitude)
+            {
+                this.magnitude = magnitude
+            }
+        }
+
+        //Makes all values on a 0 to 1 scale
+        //by dividing by the magnitude
+        Normalize()
         {
-            this.data = data
+            if (this.magnitude === undefined)
+            {
+                console.error("MissingMagnitudeError: Magnitude is undefined, thus normalization is impossible for this vector.")
+            } 
+            this.x = this.x / this.magnitude
+            this.y = this.y / this.magnitude
         }
     }
+
+    class LinkedListNode
+    {
+        /*
+        * A node for the Linked List data structure
+        */
+        next : LinkedListNode
+        previous : LinkedListNode
+        data : any
+        constructor(next, previous, data?) {
+            this.next = next
+            this.previous = previous
+
+            if (data != null)
+            {
+                this.data = data
+            }
+        }
+    }
+
+    (Global as any).LinkedListNode = LinkedListNode
+
+    class LinkedList
+    {
+        /**
+         * A class for the Linked List data structure
+        */
+        nodes : LinkedListNode[] = []
+        constructor(initialState? : LinkedListNode[]) {
+            //Need to check if it's an array else unshift won't work
+            if (initialState != null)
+            {
+                this.nodes = initialState
+            }
+        }
+
+        InsertAtHead(node : LinkedListNode)
+        {
+            this.nodes.unshift(node)
+
+            this.Update()
+        }
+
+        DeleteAtHead()
+        {
+            this.nodes.shift()
+
+            this.Update()
+        }
+
+        InsertAtEnd(node : LinkedListNode)
+        {
+            this.nodes.push(node)
+
+            this.Update()
+        }
+
+        DeleteAtEnd()
+        {
+            this.nodes.pop()
+
+            this.Update()
+        }
+
+        Search(data : any) : LinkedListNode
+        {
+            return this.nodes.find(data)
+        }
+
+        IsEmpty() : boolean
+        {
+            if (this.nodes.length === 0)
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+        }
+
+        //Updates next and previous values for nodes
+        Update()
+        {
+            for (let i = 0; i < this.nodes.length; i++)
+            {
+                if (this.nodes[i-1] != undefined && this.nodes[i-1] != null)
+                {
+                    this.nodes[i].previous = this.nodes[i-1]
+                }
+
+                if (this.nodes[i+1] != undefined && this.nodes[i+1] != null)
+                {
+                    this.nodes[i].next = this.nodes[i+1]
+                }
+            }
+        }
+    }
+    (Global as any).LinkedList = LinkedList 
 }
 
-class LinkedList
-{
-    /**
-     * A class for the Linked List data structure
-     */
-    /*
-    nodes : LinkedListNode[]
-    constructor(initialState? : LinkedListNode[]) {
-        if (initialState != null)
-        {
-            this.nodes = initialState
-        }
-    }
 
-    InsertAtHead(node : LinkedListNode)
-    {
-        this.nodes.unshift(node)
-    }
-
-    DeleteAtHead()
-    {
-        this.nodes.shift()
-    }
-
-    InsertAtEnd(node : LinkedListNode)
-    {
-        this.nodes.push(node)
-    }
-
-    DeleteAtEnd()
-    {
-        this.nodes.pop()
-    }
-
-    Search(data : any) : LinkedListNode
-    {
-        return this.nodes.find(data)
-    }
-
-    IsEmpty() : boolean
-    {
-        if (this.nodes.length === 0)
-        {
-            return true
-        }
-        else
-        {
-            return false
-        }
-    }
-}
-*/
+var finished = true
